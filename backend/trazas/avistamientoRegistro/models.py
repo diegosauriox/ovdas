37a2eb52c16c1bf967dcb7234446f7b04a210402
Benjamin Huete <b.huete01@ufromail.cl>
2022-01-12 21:50:56 -0300

    correccion de modelo para que Diego deje de molestar

--- backend/trazas/avistamientoRegistro/models.py
+++ backend/trazas/avistamientoRegistro/models.py
@@ -7,7 +7,7 @@ from identificacion.models import IdentificacionSenalModel
 
 class AvistamientoRegistroModel(models.Model):
     cod_event = models.OneToOneField(IdentificacionSenalModel, on_delete=models.CASCADE, db_column='cod_event', primary_key=True)
-    #cod_event_in = models.CharField(max_length=20)
+    cod_event_in = models.CharField(max_length=20)
     evento_macro = models.ForeignKey(EventoMacroModel, on_delete=models.CASCADE)
     t_p = models.CharField(max_length=45)
     t_s = models.CharField(max_length=45)
@@ -23,7 +23,7 @@ class AvistamientoRegistroModel(models.Model):
     descripcion = models.CharField(max_length=60, blank=True, null=True)
     componente = models.CharField(max_length=1)
     snr = models.FloatField()
-    id_tecnica = models.ForeignKey(AlgoritmoPickingModel, on_delete=models.CASCADE)
+    tecnica = models.ForeignKey(AlgoritmoPickingModel, on_delete=models.CASCADE)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField(blank=True, null=True)
 
--- backend/trazas/avistamientoRegistro/serializers.py
+++ backend/trazas/avistamientoRegistro/serializers.py
@@ -5,4 +5,4 @@ from .models import AvistamientoRegistroModel
 class AvistamientoRegistroSerializer(serializers.ModelSerializer):
     class Meta:
         model = AvistamientoRegistroModel
-        fields = ('cod_event', 'cod_event_in', 't_p', 't_s', 'coda', 'c_p', 'c_s', 'c_coda', 'polar', 'frecuencia', 'amplitud', 'autor', 'label_event', 'descripcion', 'componente', 'snr', 'id_tecnica_id', 'created_at', 'updated_at')
+        fields = ('cod_event', 'cod_event_in', 't_p', 't_s', 'coda', 'c_p', 'c_s', 'c_coda', 'polar', 'frecuencia', 'amplitud', 'autor', 'label_event', 'descripcion', 'componente', 'snr', 'tecnica', 'created_at', 'updated_at')
