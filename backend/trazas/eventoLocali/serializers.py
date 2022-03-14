ac71ddd7037537554f7b5581ff6f9e5f018e6066
Benjamin Huete <b.huete01@ufromail.cl>
2022-03-13 21:09:23 -0300

    cambios en localizacion

--- backend/trazas/eventoLocali/serializers.py
+++ backend/trazas/eventoLocali/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 from .models import EventoLocalizadoModel
+#from eventoMacro.serializers import EventoMacroSerializer
 
 
 class EventoLocaliSerializer(serializers.ModelSerializer):
+    #evento_macro_id = EventoMacroSerializer(read_only=True)
     class Meta:
         model = EventoLocalizadoModel
         fields = ('evento_loc_id', 'evento_macro_id', 'tiempo', 'lat', 'lon', 'z', 'rmse', 'major_half_axes', 'minor_half_axes', 'dz', 'gap', 'ml', 'n_fases', 'descrip', 'autor', 'created_at', 'updated_at')
@@ -14,6 +16,11 @@ class EventoLocaliSerializer2(serializers.ModelSerializer):
         fields = ('evento_macro_id', 'ml')
         
 class EventoLocaliSerializer3(serializers.ModelSerializer):
+    class Meta:
+        model = EventoLocalizadoModel
+        fields = ('evento_macro_id', 'ml','lat','lon')
+
+class EventoLocaliSerializer4(serializers.ModelSerializer):
     class Meta:
         model = EventoLocalizadoModel
         fields = ('evento_macro_id', 'ml','lat','lon')
